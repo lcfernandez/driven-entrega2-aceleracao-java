9e91577060145c030706509b3d22e183ad50f3c2
lcfernandez <lcfernandez@cos.ufrj.br>
2024-01-28 21:17:35 -0300

    feat: implement UserRepository

--- src/main/java/com/tweteroo/api/controllers/UserController.java
+++ src/main/java/com/tweteroo/api/controllers/UserController.java
@@ -5,9 +5,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.repositories.UserRepository;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+  final UserRepository userRepository;
+
+  UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
   @PostMapping
   public void createUser(@RequestBody String body) {
     System.out.println(body);
